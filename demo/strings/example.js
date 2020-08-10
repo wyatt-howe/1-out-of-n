ca3eeac7b5cf79289f91219af0ed79f7707c1a78
Wyatt Howe <wyatt@wyatthowe.net>
2020-08-10 15:44:51 -0400

    Rewire function bindings so that extra a parameters are always at the end
    IO give and get should expect arguments like:
    
    get: function (op_id, session_id, tag, extra_arg_1, extra_arg_2, extra_arg_3, ... )
    
    such that the tag is always third, not last.

--- demo/strings/example.js
+++ demo/strings/example.js
@@ -5,10 +5,12 @@ const ascii = require('./ascii.js');
  *  the methods in IO to send public messages between the two parties.
  */
 var IO = require('../io-example.js');
-const OT = require('1-out-of-n')(IO);
+const OT = require('../../index.js')(IO);
 const N = 11;
 const op_id = '1in11ot';  // OPTIONAL op_id string
 const session_id = '1,2';  // OPTIONAL string to specify participants
+const extra_arg_1 = "str_1";  // OPTIONAL extra parameters
+const extra_arg_2 = "str_2";  // (We can pass multiple, if needed for the IO.)
 
 OT.then(function (OT) {
 
@@ -29,13 +31,13 @@ OT.then(function (OT) {
     'A tenth secret!!'
   ].map(ascii.to_array);
 
-  OT.send(secrets, N, op_id, session_id);
+  OT.send(secrets, N, op_id, session_id, extra_arg_1, extra_arg_2);
 
 
   /*
    *  The receiver calls:
    */
-  OT.receive(6, N, op_id, session_id).then(function (array) {
+  OT.receive(6, N, op_id, session_id, extra_arg_1, extra_arg_2).then(function (array) {
     console.log('The chosen secret is:', ascii.to_ascii(array));
   });
 
