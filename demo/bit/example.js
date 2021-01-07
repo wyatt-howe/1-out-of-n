f5c48d65ee7edd81d904e3728b6e04c32025a9d1
Wyatt Howe <43802211+wyatt-howe@users.noreply.github.com>
2021-01-07 03:49:00 -0500

    Update example.js

--- demo/bit/example.js
+++ demo/bit/example.js
@@ -13,31 +13,47 @@ OT.then(function (OT) {
   /*
    *  The sender calls:
    *///            0  1  2  3
-  const secrets = [1, 1, 0, 1];
-  OT.send(secrets, N, op_id, session_id);
+  const secrets = [1, 0, 0, 0];
+  // OT.send(secrets, N, op_id, session_id);
+  //
+  //
+  // /*
+  //  *  The receiver calls:
+  //  */
+  // OT.receive(1, N, op_id, session_id).then(function (b) {
+  //   console.log('The chosen secret is:', b);
+  // });
+  //
+  //
+  //
+  // // Again anew, but choose 2 this time:
+  // OT.send(secrets, N, '1in4to', '3,4');
+  // OT.receive(2, N, '1in4to', '3,4').then(function (b) {
+  //   console.log('The chosen secret is:', b);
+  // });
+  // OT.send(secrets, N, '1in4too', '3,4');
+  // OT.receive(3, N, '1in4too', '3,4').then(function (b) {
+  //   console.log('The chosen secret is:', b);
+  // });
+  // OT.send(secrets, N, '1in4tooo', '3,4');
+  // OT.receive(0, N, '1in4tooo', '3,4').then(function (b) {
+  //   console.log('The chosen secret is:', b);
+  // });
 
 
-  /*
-   *  The receiver calls:
-   */
-  OT.receive(1, N, op_id, session_id).then(function (b) {
-    console.log('The chosen secret is:', b);
+
+  console.time('bench');
+  var promises = [];
+  for (let i = 0; i < 10000; i++) {
+    let c = i%N;
+    // console.log(secrets, c);
+    OT.send(secrets, N, op_id + ':' + i, session_id + ':' + i);
+    promises.push(OT.receive(c, N, op_id + ':' + i, session_id + ':' + i));
+  }
+  Promise.all(promises).then(function (bs) {
+    console.timeEnd('bench');
+    bs.map(function (b) {
+      // console.log('The chosen secret is:', b);
+    });
   });
-
-
-
-  // Again anew, but choose 2 this time:
-  OT.send(secrets, N, '1in4to', '3,4');
-  OT.receive(2, N, '1in4to', '3,4').then(function (b) {
-    console.log('The chosen secret is:', b);
-  });
-  OT.send(secrets, N, '1in4too', '3,4');
-  OT.receive(3, N, '1in4too', '3,4').then(function (b) {
-    console.log('The chosen secret is:', b);
-  });
-  OT.send(secrets, N, '1in4tooo', '3,4');
-  OT.receive(0, N, '1in4tooo', '3,4').then(function (b) {
-    console.log('The chosen secret is:', b);
-  });
-
 });
